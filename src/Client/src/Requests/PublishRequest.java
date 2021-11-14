0296767875ff67441779e87c2b5e4c31e0f25be3
nabil.alamgir <nabil.alamgir@cgi.com>
2021-11-14 15:40:32 -0500

    modified PublishRequest.java

--- src/Client/src/Requests/PublishRequest.java
+++ src/Client/src/Requests/PublishRequest.java
@@ -11,8 +11,14 @@ public class PublishRequest extends Request {
     private String clientName;
     private ArrayList<String> listOfFiles;
 
-    public PublishRequest(RequestType requestType, int reqNumber, String clientName, ArrayList<String> listOfFiles) {
-        super(requestType, reqNumber);
+    /**
+     * contstructor a request that ask to register a client
+     * @param rqNumber the id number of the request
+     * @param clientName name of client
+     * @param listOfFiles list of files for the client
+     */
+    public PublishRequest(int rqNumber, String clientName, ArrayList<String> listOfFiles) {
+        super(RequestType.PUBLISH, rqNumber);
         this.clientName = clientName;
         this.listOfFiles = listOfFiles;
     }
@@ -33,6 +39,7 @@ public class PublishRequest extends Request {
         this.listOfFiles = listOfFiles;
     }
 
+    //prints the request
     @Override
     public String toString() {
         return RequestType.PUBLISH + " " + this.getRQNumb() + " " + getClientName() + " " + getListOfFiles();
